8776a88c202c95a1e24c72321994bca28d0d19f4
Ald121 <fallsouls@hotmail.com>
2017-08-29 22:38:12 -0500

    blusas
    blusas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,28 +8,23 @@
  *
  * Main module of the application.
  */
-angular
+var app = angular
   .module('mysticApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    'ngTouch',
+    'route-segment',
+    'view-segment',
+  ]);
+  app.config(function ($routeSegmentProvider, $routeProvider,$locationProvider) {
+      $routeSegmentProvider.options.autoLoadTemplates = true; 
+      $routeSegmentProvider    
+            .when('/',    'main')        
+            .segment('main', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            }).up()
   });
