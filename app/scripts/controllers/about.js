fd331dfe601d8849a3c9d1979ed06d5926d81423
Ald121 <fallsouls@hotmail.com>
2017-08-29 21:39:36 -0500

    incio

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mysticApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mysticApp
+ */
+angular.module('mysticApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
